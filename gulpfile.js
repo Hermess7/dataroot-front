d9c49c37f07ce69c230b47f44a8b258178ff8652
Dmitriy Tkachishin <dimusei21@gmail.com>
2017-04-15 23:32:43 +0300

    add menu listing

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task('serv', function() {
     gulp.src('./build')
         .pipe(server({
             livereload: true,
-            defaultFile: 'index.html',
+            defaultFile: 'listing.html',
             directoryListing: false,
             open: false
         }));
