22d04cf50ad2b97a33086f07ded5150600a64c8f
Dmitriy Tkachishin <dimusei21@gmail.com>
2017-05-02 22:36:28 +0300

    add select

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -2,6 +2,7 @@
  * Created by User on 031 31.03.17.
  */
 $(document).ready(function(){
+    $('select.styled').customSelect();
     $('.owl-carousel').owlCarousel({
         margin:0,
         loop:true,
